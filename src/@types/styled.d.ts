f055b5d0d99d382c175bc56e5915307c26c772db
Elvis_Ribeiro Computer_home <elvis.ribeiro.deoliveira@hotmail.com>
2023-03-24 13:55:43 -0300

    Entendo as Estrura do Projeto

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+// Codigo totalmente TS, arquivo de definicao de tipo onde que possui so tipagem
+
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
